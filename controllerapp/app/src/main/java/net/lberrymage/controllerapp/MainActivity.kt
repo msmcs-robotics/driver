a99d86f101685f85cc3ee512ea76d4cbba3f60ac
lberrymage <mageelog@gmail.com>
2021-09-22 16:04:30 -0800

    Add "hello world" controller Android app
    No functionality is implemented yet - this is just setting up the
    project structure.

---
+++ controllerapp/app/src/main/java/net/lberrymage/controllerapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.lberrymage.controllerapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
